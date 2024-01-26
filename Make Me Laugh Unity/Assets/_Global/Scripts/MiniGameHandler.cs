07c09d9cbbe591b484c759dfcc0a5901a0d56a7c
Matt Burton <matthewwburton@gmail.com>
2024-01-26 15:06:50 -0600

    Initial Commit

---
+++ Make Me Laugh Unity/Assets/_Global/Scripts/MiniGameHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game1;
+using Game2;
+using Game3;
+using Game4;
+
+public class MiniGameHandler : MonoBehaviour
+{
+    public Game1.NamespaceExample game1Handler;
+    public Game2.NamespaceExample game2Handler;
+    public Game3.NamespaceExample game3Handler;
+    public Game4.NamespaceExample game4Handler;
+}
